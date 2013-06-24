2726a3c44e90229899d90f6bb1dc7b711249bb8a
clintoncontreras <ClintonC@zoovy.com>
2013-06-23 17:30:59 -0700

    Corrected magic zoom bug on product page

--- extensions/partner_magictoolbox_mzp.js
+++ extensions/partner_magictoolbox_mzp.js
@@ -127,7 +127,7 @@ var magicToolBox_mzp = function() {
 					imgName = pdata['zoovy:prod_image'+i];
 //					app.u.dump(" -> "+i+": "+imgName);
 					if(app.u.isSet(imgName))	{
-/*BEACHMALL*/			imgs += "<li><a class='MagicThumb' rel='zoom-id: prodBigImage_href_"+data.value+"; hint: false' rev='"+app.u.makeImage({'tag':0,'w':340,'h':340,'name':imgName,'b':'ffffff'})+"' href='"+app.u.makeImage({'tag':0,'w':'','h':'','name':imgName,'b':'ffffff'})+"'><img src='"+app.u.makeImage({'tag':0,'w':50,'h':50,'name':imgName,'b':'ffffff'})+"' \/><\/a><\/li>";
+/*BEACHMALL*/			imgs += "<li><a class='MagicThumb-swap' rel='zoom-id: prodBigImage_href_"+data.value+"; hint: false' rev='"+app.u.makeImage({'tag':0,'w':340,'h':340,'name':imgName,'b':'ffffff'})+"' href='"+app.u.makeImage({'tag':0,'w':'','h':'','name':imgName,'b':'ffffff'})+"'><img src='"+app.u.makeImage({'tag':0,'w':50,'h':50,'name':imgName,'b':'ffffff'})+"' \/><\/a><\/li>";
 						}
 					}
 				$tag.append(imgs);
