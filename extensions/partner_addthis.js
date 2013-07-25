99fc5783cbc1c905e70cd5925afbd93fc86779e0
Clinton Contreras <ClintonC@zoovy.com>
2013-07-25 11:55:37 -0700

    attempted fix of addThis share+ box. + versioning

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -82,7 +82,7 @@ var partner_addthis = function() {
 					+		'<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>'
 					+		'<a class="addthis_button_tweet"></a>'
 					+		'<a class="addthis_button_pinterest_pinit"></a>'
-					+		'<a class="addthis_counter addthis_pill_style"></a>'
+					+		'<a class="addthis_counter addthis_pill_style addthis_nonzero"></a>'
 					+	'</div>');
 
 				//Set URL+title for most sharing code
