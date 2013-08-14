c15f4ad348da31ebbb2eeafabc063169f3cf0c59
clintoncontreras <ClintonC@zoovy.com>
2013-08-14 11:18:33 -0700

    Reverted all fix changes

--- _itasca_app-init.js
+++ _itasca_app-init.js
@@ -116,7 +116,6 @@ app.rq.push(['templateFunction','productTemplate','onDeparts',function(P) {
 	
 
 //**BEGIN TITLE APPENDING FUNCTIONALITY**//
-				/*
 				app.rq.push(['templateFunction', 'categoryTemplate','onCompletes',function(P){
 						var title = app.data["appPageGet|"+P.navcat]['%page'].page_title;
 						app.ext.store_itasca.u.setTitle(title);
@@ -145,7 +144,6 @@ app.rq.push(['templateFunction','productTemplate','onDeparts',function(P) {
 				app.rq.push(['templateFunction', 'searchTemplate','onCompletes',function(P){
 					app.ext.store_itasca.u.setTitle();
 					}]);
-					*/
 //**END TITLE APPENDING FUNCTIONALITY**//
 
 var showDropdown = function ($tag) {
